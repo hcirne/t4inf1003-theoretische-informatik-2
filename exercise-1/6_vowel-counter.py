3f1119f18c5cbf255c81e9faf354806da530970f
hcirne <henriquepcirne@gmail.com>
2026-05-17 23:10:38 +0100

    added pseudocode for item 6

--- exercise-1/6_vowel-counter.py
+++ exercise-1/6_vowel-counter.py
@@ -1,3 +1,47 @@
+# Pseudocode
+# 
+# Algoritm VowelCounter(word)
+#
+# // This function iterates through a string and counts the frequency of each vowel.
+#
+# {
+#     a := 0; 
+#     e := 0;
+#     i := 0;
+#     o := 0;
+#     u := 0;
+# 
+#     for letter := 0 to (length of word) do
+#     {
+#         if (word[letter] = 'a') then
+#         {
+#             a = a + 1;
+#         }
+#         if (word[letter] = 'e') then
+#         {
+#             e = e + 1;
+#         }
+#         if (word[letter] = 'i') then
+#         {
+#             i = i + 1;
+#         }
+#         if (word[letter] = 'o') then
+#         {
+#             o = o + 1;
+#         }
+#         if (word[letter] = 'u') then
+#         {
+#             u = u + 1;
+#         }
+#     }
+#     write('Frequency of each vowel in:', word)
+#     write('A:', a)
+#     write('E:', e)
+#     write('I:', i)
+#     write('O:', o)
+#     write('U:', u)
+# }
+
 def VowelCounter(word):
     a = 0
     e = 0
