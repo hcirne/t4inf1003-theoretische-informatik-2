a30b795b67170d609b78cd4dfaa09ca24827aaaa
hcirne <henriquepcirne@gmail.com>
2026-05-17 19:43:48 +0100

    added pseudcode, added conversion of integers to strings, and added tests

--- exercise-1/2_palindrome.py
+++ exercise-1/2_palindrome.py
@@ -1,9 +1,32 @@
-def CheckPalindrome(word):
-	l = len(word)
-	for x in range(l // 2):
-		if word[x] != word[l - 1 - x]:
-			return False
-	return True
+# Pseudocode
+#
+# Algorithm Palindrome(input)
+#
+# // This algorithm verifies if an input (string or a number) reads the same backwward as forward.
+# 
+# {
+#     word := str(input)
+#     l := len(word);
+#     for x := 0 to len // 2 do
+#     {
+#         if (word[x] =/ word[l - 1 -x]) then
+#         {
+#             return True;
+#         }
+#     }
+#     return False;
+# }
+
+def CheckPalindrome(input):
+    word = str(input)
+    l = len(word)
+    for x in range(l // 2):
+        if word[x] != word[l - 1 - x]:
+            return False
+
+    return True
 
 print(CheckPalindrome('radar'))
 print(CheckPalindrome('boat'))
+print(CheckPalindrome(123321))
+print(CheckPalindrome(123))
