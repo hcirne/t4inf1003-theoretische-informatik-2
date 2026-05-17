575a212314d8ba4ef5485fc6f9ab6b60f1c78d19
hcirne <henriquepcirne@gmail.com>
2026-05-17 22:53:47 +0100

    added pseudocode

--- exercise-1/3_factorial.py
+++ exercise-1/3_factorial.py
@@ -1,8 +1,24 @@
+# Pseudocode
+#
+# Algorithm Factorial(n) 
+# 
+# // This function takes an input "n" and calculates the factorial 
+#
+# {
+#     result := 1;
+#     for x := 1 to (n + 1) do
+#     {
+#         result = result * x;    
+#     }
+#     return result;
+# }
+
 def Factorial(n):
     result = 1
     for x in range(1, n+1):
         result *= x
     return result
 
-print(Factorial(3))
+print(Factorial(0))
+print(Factorial(1))
 print(Factorial(5))
