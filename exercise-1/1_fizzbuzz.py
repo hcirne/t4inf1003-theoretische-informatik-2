56fcfc5b606352f8abd0488e3a298667d9a8c3fb
hcirne <henriquepcirne@gmail.com>
2026-05-17 19:24:47 +0100

    fixed indentation and added missing initial brackets

--- exercise-1/1_fizzbuzz.py
+++ exercise-1/1_fizzbuzz.py
@@ -6,25 +6,27 @@
 # // "Buzz" for multiples of 5 and "FizzBuzz" for mutiples of both.
 # // The while loop, altough not as code effiecient as the "for loop" for this scenario, was used to simply practice writing pseudocode.
 #
+# {
 # i := 1
 # while i < 101 do
-#   if (i % 3 = 0) AND (i % 5 = 0) then
-#   {
-#     write(i, "FizzBuzz")
-#   }
-#   if (i % 3 = 0) then
-#   {
-#     write(i, "Fizz")
-#   }
-#   if (i % 5 = 0) then
-#   {
-#     write(i, "Buzz")
-#   }
-#   else
-#   {
+#     if (i % 3 = 0) AND (i % 5 = 0) then
+#     {
+#         write(i, "FizzBuzz")
+#     }
+#     if (i % 3 = 0) then
+#     {
+#         write(i, "Fizz")
+#     }
+#     if (i % 5 = 0) then
+#     {
+#         write(i, "Buzz")
+#     }
+#     else
+#     {
 #     write(i)
-#   }
-#   i := i + 1
+#     }
+#     i := i + 1
+# }
 
 def FizzBuzz():
     i = 1
