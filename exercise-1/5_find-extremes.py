85583dd25290fa73f653d91e549ce152f7cc8fcd
hcirne <henriquepcirne@gmail.com>
2026-05-17 23:03:24 +0100

    added pseudocode for item 5

--- exercise-1/5_find-extremes.py
+++ exercise-1/5_find-extremes.py
@@ -1,3 +1,26 @@
+# Pseudocode
+#
+# Algorithm FindExtremes(array)
+#
+# // This algorithm takes an unsorted list "array" and returns the minimum and maximum values in it.
+#
+# {
+#     minimum := array[0];
+#     maximum := array[0];
+#     for item := 0 to (length of array) do
+#     {
+#         if (item < minimum) then
+#         {
+#             minimum := item;
+#         }
+#         if (item > maximum) then
+#         {
+#             maximum := item;
+#         }
+#     }
+#     return minimum, maximum;
+# }
+
 def FindExtremes(array):
     minimum = array[0]
     maximum = array[0]
