fe3b57577a5b4cb0bb22b5b7321443eac5bea19e
hcirne <henriquepcirne@gmail.com>
2026-05-17 19:07:34 +0100

    added pseudocode, fixed loop to start from 1 and end in 100, and added print statement for every number iteration

--- exercise-1/1_fizzbuzz.py
+++ exercise-1/1_fizzbuzz.py
@@ -1,12 +1,42 @@
+# Pseudocode
+#
+# Algorithm FizzBuzz()
+#
+# // This function iterates from 1 to 100, printing the word "Fizz" for multiples of 3, 
+# // "Buzz" for multiples of 5 and "FizzBuzz" for mutiples of both.
+# // The while loop, altough not as code effiecient as the "for loop" for this scenario, was used to simply practice writing pseudocode.
+#
+# i := 1
+# while i < 101 do
+#   if (i % 3 = 0) AND (i % 5 = 0) then
+#   {
+#     write(i, "FizzBuzz")
+#   }
+#   if (i % 3 = 0) then
+#   {
+#     write(i, "Fizz")
+#   }
+#   if (i % 5 = 0) then
+#   {
+#     write(i, "Buzz")
+#   }
+#   else
+#   {
+#     write(i)
+#   }
+#   i := i + 1
+
 def FizzBuzz():
-    i = 0
-    while i < 100:
+    i = 1
+    while i < 101:
         if ((i % 3 == 0) and (i % 5 == 0)):
             print(i, 'FizzBuzz')
         elif (i % 3 == 0):
             print(i, 'Fizz')
         elif (i % 5 == 0):
             print(i, 'Buzz')
+        else:
+            print(i)
         i += 1
 
 FizzBuzz()
